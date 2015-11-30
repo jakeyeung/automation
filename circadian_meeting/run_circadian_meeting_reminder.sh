bc2964e2df42a6f27b1c22b5aceae8894fdac344
Jake Yeung <jakeyeung@gmail.com>
2015-11-30 14:06:59 +0100

    do checks

--- circadian_meeting/run_circadian_meeting_reminder.sh
+++ circadian_meeting/run_circadian_meeting_reminder.sh
@@ -4,12 +4,18 @@
 # Run it 
 # 2015-11-30
 
-pyscript="/Users/yeung/scripts/automation/circadian_meeting/make_send_auto_email.py"
+pyscript="/Users/yeung/scripts/automation/make_send_auto_email.py"
 sched="/Users/yeung/scripts/automation/circadian_meeting/circadian_meeting.schedule"
 email="/Users/yeung/scripts/automation/circadian_meeting/circadian_meeting.emails"
 # email="/Users/yeung/scripts/automation/circadian_meeting/circadian_meeting.emails.test"
 msg="/Users/yeung/scripts/automation/circadian_meeting/circadian_meeting.message"
 sub="/Users/yeung/scripts/automation/circadian_meeting/circadian_meeting.subject"
+
+[[ ! -e $pyscript ]] && echo "$pyscript not found, exiting" && exit 1
+[[ ! -e $sched ]] && echo "$sched not found, exiting" && exit 1
+[[ ! -e $email ]] && echo "$email not found, exiting" && exit 1
+[[ ! -e $msg ]] && echo "$msg not found, exiting" && exit 1
+[[ ! -e $sub ]] && echo "$sub not found, exiting" && exit 1
 
 python $pyscript --schedule $sched --emails $email --message $msg --subject $sub
 
